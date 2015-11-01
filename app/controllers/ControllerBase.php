7be44a82938a84f74082b21285911a7ac713f3c7
Munoz Daniel <munozda87@hotmail.com>
2015-11-01 11:06:32 -0300

    Inicializando los controladores index y base.

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -4,5 +4,23 @@ use Phalcon\Mvc\Controller;
 
 class ControllerBase extends Controller
 {
+    protected function initialize()
+    {
+        $this->tag->prependTitle('FARMACIA OESTE | ');
+        $this->view->setTemplateAfter('principal');
 
+    }
+
+    protected function redireccionar($uri)
+    {
+        $uriParts = explode('/', $uri);
+        $params = array_slice($uriParts, 2);
+        return $this->dispatcher->forward(
+            array(
+                'controller' => $uriParts[0],
+                'action' => $uriParts[1],
+                'params' => $params
+            )
+        );
+    }
 }
